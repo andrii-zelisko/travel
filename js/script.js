dc4602b559aa1356417fd3c7f75a9904f7614a0e
andrii-zelisko <andrii.zelisko@gmail.com>
2019-11-05 00:12:00 +0200

    add timer

--- js/script.js
+++ js/script.js
@@ -35,4 +35,44 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    // Timer
+    let deadline = '2019-11-05';
+
+    function getTimeRemaining( endTime ) {
+        let t = Date.parse( endTime ) - Date.parse( new Date() ),
+            seconds = Math.floor( t / 1000 % 60 ),
+            minutes = Math.floor( t / 1000 / 60 % 60 ),
+            hours = Math.floor( t / ( 1000 * 60 * 60 ) );
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    }
+
+    function setClock( id, endTime ) {
+        let timer = document.getElementById( id ),
+            hours = timer.querySelector( '.hours' ),
+            minutes = timer.querySelector( '.minutes' ),
+            seconds = timer.querySelector( '.seconds' );
+            let timeInterval = setInterval( updateClock, 1000 );
+
+        function updateClock() {
+            let t = getTimeRemaining( endTime );
+            hours.textContent = t.hours / 10 >= 1 ? t.hours : `0${t.hours}`;
+            minutes.textContent = t.minutes / 10 >= 1 ? t.minutes : `0${t.minutes}`;
+            seconds.textContent = t.seconds / 10 >= 1 ? t.seconds : `0${t.seconds}`;
+
+            if( t.total <= 0 ) {
+                clearInterval( timeInterval );
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock( 'timer', deadline );
 });
